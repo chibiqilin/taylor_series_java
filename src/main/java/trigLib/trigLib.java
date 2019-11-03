c1e7ab2189a1410d382c3591fc5c7ff364cefc7e
ChibiQilin <56411355+chibiqilin@users.noreply.github.com>
2019-11-02 21:56:09 -0400

    v1.00
    Added abstract trigLib.java with empty methods sin, cos, tan, sec, csc, cot returning null.
    
    Test cases need to be formally written to cover all extreme cases.

---
+++ src/main/java/trigLib/trigLib.java
@@ -0,0 +1,27 @@
+package trigLib;
+
+public abstract class trigLib {
+    public static Double sin(Double radians){
+        return null;
+    }
+
+    public static Double cos(Double radians){
+        return null;
+    }
+
+    public static Double tan(Double radians){
+        return null;
+    }
+
+    public static Double sec(Double radians){
+        return null;
+    }
+
+    public static Double csc(Double radians){
+        return null;
+    }
+
+    public static Double cot(Double radians){
+        return null;
+    }
+}
--- src/test/java/TestSuite_1.java
+++ src/test/java/TestSuite_1.java
@@ -1,12 +1,38 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+import trigLib.*;
 
 /**
  *
  */
 public class TestSuite_1 {
+
+
+    //Create output file
+    @Before
+    public void createOutput() {
+        //TODO Generate output
+    }
+
+    //Close output file
+    @After
+    public void closeOutput() {
+    }
+
     @Test
     public void Test1(){
-        System.out.println("Test 1");
+        Double test = 90.0;
+        Double result = trigLib.sin(test);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void Test2(){
+        Double test = 70.0;
+        Double result = trigLib.sin(test);
+        assertNotNull(result);
     }
 
 }
