352363425f8422665147b535a29017336c363236
sailosha <powerer@qq.com>
2019-11-03 13:14:15 -0500

    Update SinTest.java
    update test case for the sin

--- src/main/java/trigLib/SinTest.java
+++ src/main/java/trigLib/SinTest.java
@@ -1,4 +1,4 @@
-package trigLib;
+package trigLib;,
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
-import static trigLib.trigLib.sin;
+import static trigLib.trigLib.trigLib.sin;
 
 @RunWith(Parameterized.class)
 public class SinTest {
@@ -30,10 +30,19 @@ public class SinTest {
     @Parameterized.Parameters
     public static Collection data(){
         return Arrays.asList(new Object[][] {
-                {0.0, 0.0},
-                {90.0, 0.0},
-                {Double.POSITIVE_INFINITY, Double.NaN},
-                {Double.NEGATIVE_INFINITY, Double.NaN}
+            {0.0, 0.0},
+            {90.0, 1},
+            {30.0,0.5},
+            {45.0,0.70710},
+            {60,0.86602},
+            {120,0.86602},
+            {150,0.5},
+            {180,0},
+            {270,-1},
+            {360,0},
+            {Double.POSITIVE_INFINITY, Double.NaN},
+            {Double.NEGATIVE_INFINITY, Double.NaN}
+            //TODO: make catch the error input
         } );
     }
 
