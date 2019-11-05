edafe39041694dd05ad6a89b9f981ae8b13d434e
sailosha <powerer@qq.com>
2019-11-05 16:55:52 -0500

    implement cosin
    Implement cosin function and pass unit test

--- src/main/java/trigLib/trigLib.java
+++ src/main/java/trigLib/trigLib.java
@@ -2,9 +2,31 @@ package trigLib;
 
 public abstract class trigLib {
 
-    public static Double cos(Double degrees) {
-        return null;
-    }
+	/*
+	 * cosine function
+	 */
+	public static double cos(double target) {
+		// angle to radians
+
+		// change it later
+
+		// TODO: use value to isntaed of PI
+		double rad = target * 1. / 180. * Math.PI;
+		// the first element of the taylor series
+		double sum = 1.0;
+
+		// add them up until a certain precision (eg. 10)
+		for (int i = 1; i <= 14; i++) {
+			if (i % 2 == 0)
+
+				sum += power(rad, 2 * i) / factorial(2 * i );
+			else
+				sum -= power(rad, 2 * i) / factorial(2 * i );
+		}
+
+		return sum;
+
+	}	
 
     public static Double tan(Double degrees) {
         return null;
