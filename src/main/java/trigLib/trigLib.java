59e726b8d39efc3412313b52455b0f1d322ee908
sailosha <powerer@qq.com>
2019-11-05 19:19:33 -0500

    implment sec
    test unit pass

--- src/main/java/trigLib/trigLib.java
+++ src/main/java/trigLib/trigLib.java
@@ -49,9 +49,27 @@ public abstract class trigLib {
 	}
 
 
-    public static Double sec(Double degrees) {
-        return null;
-    }
+	/*
+	 * sec function
+	 */
+	public static double sec(double target) {
+		double numerator = 1;
+		double denominator = cos(target);
+		
+		double value = numerator/denominator;
+		if (denominator<= 0.00001 && denominator >0.0 )
+			return value=Double.NaN;
+		else if( denominator>= -0.00001 && denominator <0.0){
+			System.out.println("warning it is works");
+			return value=Double.NaN;
+		}else if( denominator ==0){
+			return value=Double.NaN;
+		}else if ( denominator == Double.NaN){
+			return value=Double.NaN;
+		}
+		
+		return value;
+	}
 
     public static Double csc(Double degrees) {
         return null;
